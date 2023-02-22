10174260c4494e4ce2b15eae091b2dfacb6172ab
bertrandsimon <trandber@hotmail.com>
2023-02-22 19:02:27 +0100

    card component html and css started

--- components/homepage/Card.js
+++ components/homepage/Card.js
@@ -5,9 +5,24 @@ function Card() {
 
   return (
 
-    <div className={styles.container}>
+    <div className={styles.cardWrapper}>
 
-        <h1 className={styles.title}>Card</h1>
+        <h5 className={styles.title}>h5 Headline</h5>
+
+        <div className={styles.tagWrapper}>
+          <div className={styles.tag1}><span>cdi</span></div>
+          <div className={styles.tag2}><span>78</span></div>
+        </div>
+
+        <div className={styles.txt}>
+          <span>ion ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
+        </div>
+
+        <div className={styles.bottomWrapper}>
+          <div className={styles.line}></div>
+          <div><span>posté : 1 jour</span></div>
+        </div>
+
       
     </div>
 
--- components/homepage/TopOffersWrapper.js
+++ components/homepage/TopOffersWrapper.js
@@ -1,7 +1,7 @@
 import styles from '../../styles/TopOffersWrapper.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowTurnDown } from '@fortawesome/free-solid-svg-icons';
-
+import Image from 'next/image';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -25,17 +25,17 @@ function TopOffersWrapper() {
 
     <div>
         <div className={styles.title}>
-            <span className={styles.subtitle1}>Les offres</span> <span className={styles.subtitle2}> du moment</span><span><FontAwesomeIcon icon={faArrowTurnDown} className={styles.arrow}/></span>
+            <span className={styles.subtitle1}>Les offres</span> <span className={styles.subtitle2}> du moment</span><Image src="/images/arrow2.jpg" width={19} height={27} className={styles.arrow}/>
         </div>
 
         <div className={styles.container}>
             <div>
-              
+
             </div>
-           
+
         </div>
-        
-      
+
+
     </div>
 
   );
